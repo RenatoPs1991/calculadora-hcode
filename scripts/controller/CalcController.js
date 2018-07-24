7c312a057a3ad0b1fdd15a29842d7508c0ac5c3f
Renato Pereira <renatops1991@gmail.com>
2018-07-24 12:05:33 -0300

    Swicth com os valores parametrizados

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,8 +1,9 @@
 class CalcController {
 
-//   Método construtor
+    //Método construtor
     constructor() {
 
+        this._operation = [];
         this._locale = "pt-BR";
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -13,7 +14,7 @@ class CalcController {
 
     }
 
-//    Método de inicialização para manipulação do DOM
+    //Método de inicialização para manipulação do DOM
     initialize() {
         this.setDisplayDateTime();
         setInterval(() => {
@@ -22,18 +23,103 @@ class CalcController {
 
     }
 
-//    Método para selecionar os botões para chamar o valor deles
+    //Método de criação de eventos
+    addEventListenerAll(element, events, fn) {
+
+        events.split(" ").forEach(event => {
+            element.addEventListener(event, fn, false);
+        });
+
+    }
+
+    //Método para limpar todos os campos
+    clearAll() {
+        this._operation = [];
+    }
+
+    //Método para limpar o ultimo campo
+    clearEntry() {
+        this._operation.pop();
+    }
+
+    //Método para incluir um dado no array
+    addOperation(value) {
+        this._operation.push(value);
+    }
+
+    // Método para mostrar o error
+    setError() {
+        this.displayCalc = "Error";
+    }
+
+    //Método para identificar cada tecla e retornar uma ação de acordo com o nome da class
+    execBtn(value) {
+        switch (value) {
+            case 'ac':
+                this.clearAll();
+                break;
+
+            case 'ce':
+                this.clearEntry();
+                break;
+
+            case 'soma':
+                break;
+
+            case 'subtracao':
+                break;
+
+            case 'divisao':
+                break;
+
+            case 'multiplicacao':
+                break;
+
+            case 'porcento':
+                break;
+
+            case 'igual':
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+                break;
+        }
+    }
+
+    //Método para selecionar os botões para chamar o valor deles
     initButtonsEvents() {
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
         buttons.forEach((btn, index) => {
-            btn.addEventListener('click', e => {
-                console.log(btn.className.baseVal.replace("btn-", ""));
+            this.addEventListenerAll(btn, 'click drag', e => {
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+
+                this.execBtn(textBtn);
+            });
+
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+                btn.style.cursor = "pointer";
             });
         });
+
     }
 
-//    Método para pegar a hora dinamicamente
+    //Método para pegar a hora dinamicamente
     setDisplayDateTime() {
 
         this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
@@ -45,7 +131,7 @@ class CalcController {
 
     }
 
-//    Métodos get/set
+    //Métodos get/set
     get displayTime() {
         return this._timeEl.innerHTML;
     }
